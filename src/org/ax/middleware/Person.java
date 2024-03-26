16a2b0be459746b91d1f3be7e5aabaa2999ea5c2
AlexMartGong <155708055+AlexMartGong@users.noreply.github.com>
2024-03-26 13:29:29 -0600

    Start of the project

---
+++ src/org/ax/middleware/Person.java
@@ -0,0 +1,40 @@
+package org.ax.middleware;
+
+public class Person {
+    private int id;
+    private String name;
+    private String address;
+    private String phoneNumber;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
